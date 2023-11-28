50dd00638430f7b146a9d10261b67bdf4ba88c6a
Thibeau De Baene <dbthibeau@gmail.com>
2023-11-28 12:13:12 +0100

    oefeningen 7.3

--- src/Child.java
+++ src/Child.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Child {
+public class Child {
+    private String name;
+    private int age;
+
+    public Child(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() > 4 && name.length() < 81) {
+            this.name = name;
+        } else {
+            System.out.println("De naam is te lang/kort.");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 16 && age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("De leeftijd is te oud/jong.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Child child = new Child("Kjento", 9);
+        System.out.println("Naam: " + child.getName());
+        System.out.println("Leeftijd: " + child.getAge());
+        child.setName("Robbie");
+        child.setAge(8);
+        System.out.println("Nieuwe naam: " + child.getName());
+        System.out.println("Nieuwe leeftijd: " + child.getAge());
+    }
+
+    public void celebrateBirthday() {
+        age++;
+        System.out.println("Proficiat!");
+
+    }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main2 {
+public class Main2 {
+    public static void main(String[] args) {
+        Child child = new Child("maxim", 9);
+
+
+        System.out.println("Naam: " + child.getName());
+        System.out.println("Leeftijd: " + child.getAge());
+
+
+        child.setName("davino");
+
+
+        System.out.println("Naam na update: " + child.getName());
+
+
+        child.celebrateBirthday();
+
+
+        System.out.println("Leeftijd na verjaardag: " + child.getAge());
+    }
+
 }
