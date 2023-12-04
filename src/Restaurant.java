6f52bd9c6358af673d39d17c5f3e027bcef3198e
Thibeau De Baene <dbthibeau@gmail.com>
2023-12-04 10:15:37 +0100

    oefeningen eerste semester.iml

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -6,8 +6,8 @@ public class Restaurant {
         spaghetti.kindOfMeal = "hoofdgerecht";
         spaghetti.vegetarian = true;
         System.out.println("Naam van het gerecht: " + spaghetti.name);
-        System.out.println(spaghetti.time + "min");
-        System.out.println("hoofdgerecht");
+        System.out.println("Tijd: " + spaghetti.time + "min");
+        System.out.println("Type gerecht: " + spaghetti.kindOfMeal);
         if (spaghetti.vegetarian = true) {
             System.out.println("Ja");
         } else {
---
+++ src/SquaredNumberCounter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SquaredNumberCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dit programma verheft getallen tot de tweede macht. ");
+        System.out.println("Tot en met welk getal wilt u de berekening uitvoeren? ");
+        int number = scanner.nextInt();
+        if (int number >= 0);
+    }
+}
